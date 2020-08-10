de58e30210d9bad29bce3fff3baca3a2554a66c7
jgersain <jhersein@gmail.com>
2020-08-10 16:04:00 -0500

    variables

---
+++ 01-variables.rb
@@ -0,0 +1,25 @@
+# declarar una variable
+# la convecnion es en minusculas y con un guin bajo
+edad_cliente = 5
+x = 5
+
+x.class     # tipo de objeto
+x.methods   # muestra los metodos disponibles, ya que en ruby todo es un objeto 
+x.odd?      # usando el metodo para saber si un número es impar
+x.even?     # metodo para saber si un número es par
+
+# Ruby es un lenguaje muy expresivo, cercano al inglés
+# Cuando un método lleva el interrogante es por que seguramente retornara un booleano
+
+# Cuenta con las operaciones basicas
+# Si se desea un resultado con decimales, se debe de incorporar al menos un número como decimal 
+x + 5   # suma
+x - 5   # resta
+x / 5   # división
+x * 5   # multiplicacion
+x ** 5  # potenciación
+
+
+
+
+
